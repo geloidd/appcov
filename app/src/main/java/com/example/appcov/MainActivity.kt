645899b80b6dc65fe3586203c226110fa4b65f61
geloidd <evlakhov.oleg11@gmail.com>
2021-12-05 22:13:32 +0300

    initial commit

--- app/src/main/java/com/example/appcov/MainActivity.kt
+++ app/src/main/java/com/example/appcov/MainActivity.kt
@@ -1,24 +1,50 @@
 package com.example.appcov
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.ParcelFileDescriptor.open
+import android.system.Os.open
+import android.view.MenuItem
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import com.example.appcov.databinding.ActivityMainBinding
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity: AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerlayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            it.isChecked = true
+            when (it.itemId) {
+
+                R.id.c_1 -> replaceFragment(ProfileFragment(), it.title.toString())
+                R.id.c_2 -> replaceFragment(DiaryFragment(), it.title.toString())
+//                R.id.c_2 -> startActivity(Intent(this, WeekViewActivity::class.java))
+                R.id.c_3 -> Toast.makeText(this, "Id cat_3", Toast.LENGTH_SHORT).show()
+                R.id.c_4 -> finish()
+
+            }
+            true
+        }
 
     }
     fun runGude(view: View) {
@@ -28,4 +54,24 @@ class MainActivity: AppCompatActivity() {
             drawer_layout.openDrawer(GravityCompat.START)
         }
     }
+    private fun replaceFragment(fragment: Fragment, title: String){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+        drawerlayout.closeDrawers()
+        setTitle(title)
+
+
+    }
+
+override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
--- app/src/main/java/com/example/appcov/SignInActivity.kt
+++ app/src/main/java/com/example/appcov/SignInActivity.kt
@@ -1,5 +1,4 @@
 package com.example.appcov
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,7 +6,6 @@ import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.appcov.databinding.ActivitySignInBinding
-import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -16,16 +14,18 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import java.util.zip.CheckedOutputStream
 
 class SignInActivity : AppCompatActivity() {
     lateinit var auth: FirebaseAuth
     lateinit var launcher: ActivityResultLauncher<Intent>
     lateinit var binding: ActivitySignInBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         auth = Firebase.auth
         launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
@@ -34,10 +34,7 @@ class SignInActivity : AppCompatActivity() {
                 if (account != null) {
                     firebaseAuthWithGoogle(account.idToken!!)
                 }
-                else{
-                    val i = Intent(this, Poll::class.java)
-                    startActivity(i)
-                }
+
             } catch (e: ApiException) {
                 Log.d("MyLog", "Api exception")
             }
@@ -62,14 +59,12 @@ class SignInActivity : AppCompatActivity() {
     private fun signInWithGoogle() {
         val signInClient = getClient()
         launcher.launch(signInClient.signInIntent)
-
     }
 
     private fun firebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential).addOnCompleteListener {
             if (it.isSuccessful) {
-                Log.d("MyLog", "Google signIn done")
                 checkAuthState()
             } else {
                 Log.d("MyLog", "Google signIn error")
@@ -81,13 +76,9 @@ class SignInActivity : AppCompatActivity() {
         if (auth.currentUser != null) {
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
+//            finish()
         }
-        else if(auth.currentUser == null){
-            val i = Intent(this, Poll::class.java)
-            startActivity(i)
-        }
-
-
     }
+
 
 }
